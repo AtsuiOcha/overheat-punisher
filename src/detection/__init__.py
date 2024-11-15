a4020e2ae2a969b16644e9b4a94d79abf795fedc
Oskar Hernandez <oskadez@gmail.com>
2024-11-15 14:41:26 -0800

    detect_kill feed in hud_detection

--- src/detection/__init__.py
+++ src/detection/__init__.py
@@ -1,2 +1,1 @@
-from .hud_detection import detect_kill_feed, detect_player_count, detect_player_position
-__all__ = ["detect_kill_feed", "detect_player_count", "detect_player_position"]
+from .hud_detection import *

1307d7bdc0eb315ae0481d713df9ee48a6df5f56
Oskar Hernandez <oskadez@gmail.com>
2025-07-26 14:09:23 -0700

    remove basic logging

--- src/main.py
+++ src/main.py
@@ -1,17 +1,7 @@
-import logging
-
-# configure root logger
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-     handlers=[
-        logging.StreamHandler(),  # Log to the console
-        logging.FileHandler('app.log', mode='w'),  # Log to a file (overwrite each run)
-    ],
-)
-
 def main():
     pass
 
+
 if __name__ == "__main__":
     main()
+

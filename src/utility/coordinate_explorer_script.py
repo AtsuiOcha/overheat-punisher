0a1d8956b5a70d71528b6131fa6702c0fd1c9d34
Oskar Hernandez <oskadez@gmail.com>
2025-07-26 14:08:59 -0700

    fix coordinate explorer

--- src/utility/coordinate_explorer_script.py
+++ src/utility/coordinate_explorer_script.py
@@ -1,21 +1,39 @@
+from importlib.resources import files
+from typing import Any
+
 import cv2
+import numpy as np
 
-# load image
-image_path = "../assets/game_scenarios/kill_feed_death.png" # please change this asset path if you want to explore new image
-image = cv2.imread(image_path)
+from src.assets import game_scenarios
 
-# global variables
+# Global variables
 start_x, start_y = None, None
 
-def get_coords(event, x, y, flags, param):
+
+def get_coords(event: int, x: int, y: int, flags: int, param: Any) -> None:
     global start_x, start_y
     if event == cv2.EVENT_LBUTTONDOWN:
         start_x, start_y = x, y
         print(f"Coords: ({start_x}, {start_y})")
 
-cv2.namedWindow("Screenshot")
-cv2.setMouseCallback("Screenshot", get_coords)
 
-cv2.imshow("Screenshot",  image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def main():
+    # Load image from package resources
+    resource = files(game_scenarios).joinpath("kill_feed_death.png")
+
+    with resource.open("rb") as f:
+        image = cv2.imdecode(
+            buf=np.frombuffer(f.read(), np.uint8), flags=cv2.IMREAD_COLOR
+        )
+
+    # Setup window
+    cv2.namedWindow("Screenshot")
+    cv2.setMouseCallback("Screenshot", get_coords)
+    cv2.imshow("Screenshot", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
+

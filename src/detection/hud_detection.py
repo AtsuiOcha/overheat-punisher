5f97358dec1ebcc4d792d5c9f0fba3dbf810a280
Oskar Hernandez <oskadez@gmail.com>
2025-07-26 14:08:05 -0700

    fix type hinting

--- src/detection/hud_detection.py
+++ src/detection/hud_detection.py
@@ -1,16 +1,19 @@
 import os
+from typing import Any, cast
 
 import cv2
 import easyocr
 import numpy as np
+from cv2.typing import MatLike
 from loguru import logger
 
 
 VARIANCE_THRESHOLD = 800
 MATCH_THRESHOLD = 0.9
+KILL_FEED_TRIGGER = "KILLED BY"
 
 
-def detect_kill_feed(frame: np.ndarray) -> list:
+def detect_kill_feed(frame: MatLike) -> list[tuple[str, str]]:
     """detects kill feed events from the provided game frame"""
     # region of interest
     x1, y1 = 1420, 90  # top left
@@ -18,13 +21,13 @@ def detect_kill_feed(frame: np.ndarray) -> list:
     roi = frame[y1:y2, x1:x2]
 
     # OCR works better on grayscale
-    gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    gray_roi = cv2.cvtColor(src=roi, code=cv2.COLOR_BGR2GRAY)
 
     # intialize easyOCR reader
-    reader = easyocr.Reader(["en"], gpu=False)  # set gpu = True if have gpu
+    reader = easyocr.Reader(lang_list=["en"], gpu=False)
 
     # values are maybe it can help us split guns
-    text_res = reader.readtext(gray_roi, detail=0)  # detail=0 means only text
+    text_res = cast(list[str], reader.readtext(image=gray_roi, detail=0))
 
     # kill feed should have even length
     if len(text_res) % 2 != 0:
@@ -35,11 +38,11 @@ def detect_kill_feed(frame: np.ndarray) -> list:
     return kill_feed
 
 
-def is_player_dead(frame: np.ndarray) -> bool:
+def is_player_dead(frame: MatLike) -> bool:
     """detects if player is dead.
 
     Args:
-        frame (np.ndarray) current frame capture of gameplay
+        frame (MatLike) current frame capture of gameplay
 
     Returns:
         bool: True if player is dead, False otherwise
@@ -49,25 +52,22 @@ def is_player_dead(frame: np.ndarray) -> bool:
     x2, y2 = 1900, 800  # bottom right
     roi = frame[y1:y2, x1:x2]
 
-    gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    gray_roi = cv2.cvtColor(src=roi, code=cv2.COLOR_BGR2GRAY)
 
     # intialize easyOCR reader
-    reader = easyocr.Reader(["en"], gpu=False)  # set gpu = True if have gpu
+    reader = easyocr.Reader(lang_list=["en"], gpu=False)
 
-    text_res = reader.readtext(gray_roi, detail=0)
+    text_res = cast(list[str], reader.readtext(image=gray_roi, detail=0))
 
     # check for existance of 'KILLED BY'
-    for text in text_res:
-        if "KILLED BY" in text:
-            return True
-    return False
+    return any(KILL_FEED_TRIGGER in text for text in text_res)
 
 
-def detect_round_info(frame: np.ndarray) -> tuple | None:
+def detect_round_info(frame: MatLike) -> tuple[int, int, str] | None:
     """detects game state information from UI components
 
     Args:
-        frame (np.ndarray): current frame capture of the gameplay
+        frame (MatLike): current frame capture of the gameplay
 
     Returns:
         tuple: (current_round, round_time, current_score) or None
@@ -92,26 +92,25 @@ def detect_round_info(frame: np.ndarray) -> tuple | None:
     x2, y2 = 1120, 80  # bottom right
     roi = frame[y1:y2, x1:x2]
 
-    gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    gray_roi = cv2.cvtColor(src=roi, code=cv2.COLOR_BGR2GRAY)
 
     # intialize easyOCR reader
-    reader = easyocr.Reader(["en"], gpu=False)  # set gpu = True if have gpu
+    reader = easyocr.Reader(lang_list=["en"], gpu=False)  # set gpu = True if have gpu
 
-    text_res = reader.readtext(gray_roi, detail=0)
-    logging.info("round info text_res= %s", text_res)
+    text_res = cast(list[str], reader.readtext(gray_roi, detail=0))
 
     if len(text_res) == 3:
-        round_time = fix_ocr_time_format(str(text_res[1]))
-        cur_round = int(text_res[0]) + int(text_res[2]) + 1  # type: ignore
+        round_time = fix_ocr_time_format(time_str=text_res[1])
+        cur_round = int(text_res[0]) + int(text_res[2]) + 1
         score = f"{text_res[0]} - {text_res[2]}"
 
         return (cur_round, round_time, score)
     else:
-        logging.error("round info reader detected invalid hud format: %s", text_res)
+        logger.error("round info reader detected invalid hud format: %s", text_res)
         return None
 
 
-def detect_agent_icons(frame: np.ndarray) -> tuple:
+def detect_agent_icons(frame: MatLike) -> tuple[list[str], list[str]]:
     """given a frame during game will return a 2d array with the characters on each team
 
     output: [[agent_t1, agent2_t1, ...], [agent_t2, agent2_t2, ...]]
@@ -125,17 +124,17 @@ def detect_agent_icons(frame: np.ndarray) -> tuple:
     assets_folder = os.path.abspath(assets_folder)
 
     # convert frame to grayscale
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame = cv2.cvtColor(src=frame, code=cv2.COLOR_BGR2GRAY)
 
     # team 1
-    team1_agents = []
+    team1_agents: list[str] = []
 
     # intial ROI
     x1, y1 = 435, 30  # top left
     x2, y2 = 500, 80  # bottom right
 
     for _ in range(5):
-        roi = gray_frame[y1:y2, x1:x2]
+        roi = cast(np.ndarray[Any, Any], gray_frame[y1:y2, x1:x2])
         # handle empty ROI (dead agents)
         if np.var(roi) < VARIANCE_THRESHOLD:
             roi_str = f"Top-left({x1}, {y1}) Bottom-right({x2}, {y2})"
@@ -164,12 +163,17 @@ def detect_agent_icons(frame: np.ndarray) -> tuple:
                     agent_template[:, :, :3],
                     agent_template[:, :, 3],
                 )
-                agent_template = cv2.cvtColor(agent_template, cv2.COLOR_BGR2GRAY)
+                agent_template = cv2.cvtColor(
+                    src=agent_template, code=cv2.COLOR_BGR2GRAY
+                )
                 mask = alpha
 
                 # template match
                 result = cv2.matchTemplate(
-                    roi, agent_template, cv2.TM_CCORR_NORMED, mask=mask
+                    image=roi,
+                    templ=agent_template,
+                    method=cv2.TM_CCORR_NORMED,
+                    mask=mask,
                 )
                 loc = np.where(result >= MATCH_THRESHOLD)
                 # update max match
@@ -185,14 +189,14 @@ def detect_agent_icons(frame: np.ndarray) -> tuple:
         x2 += 65
 
     # team 2
-    team2_agents = []
+    team2_agents: list[str] = []
 
     # intial ROI
     x1, y1 = 1423, 30  # top left
     x2, y2 = 1488, 80  # bottom right
 
     for _ in range(5):
-        roi = gray_frame[y1:y2, x1:x2]
+        roi = cast(np.ndarray[Any, Any], gray_frame[y1:y2, x1:x2])
         # handle empty ROI (dead agents)
         if np.var(roi) < VARIANCE_THRESHOLD:
             roi_str = f"Top-left({x1}, {y1}) Bottom-right({x2}, {y2})"
@@ -221,12 +225,17 @@ def detect_agent_icons(frame: np.ndarray) -> tuple:
                     agent_template[:, :, :3],
                     agent_template[:, :, 3],
                 )
-                agent_template = cv2.cvtColor(agent_template, cv2.COLOR_BGR2GRAY)
+                agent_template = cv2.cvtColor(
+                    src=agent_template, code=cv2.COLOR_BGR2GRAY
+                )
                 mask = alpha
 
                 # template match
                 result = cv2.matchTemplate(
-                    roi, agent_template, cv2.TM_CCORR_NORMED, mask=mask
+                    image=roi,
+                    templ=agent_template,
+                    method=cv2.TM_CCORR_NORMED,
+                    mask=mask,
                 )
                 loc = np.where(result >= MATCH_THRESHOLD)
 
@@ -245,4 +254,3 @@ def detect_agent_icons(frame: np.ndarray) -> tuple:
 
     # sort the agents for easier testing
     return (sorted(team1_agents), sorted(team2_agents))
-

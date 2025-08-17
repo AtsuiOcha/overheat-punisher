b51be1da4d8539d18cd6033c324d145434301220
Oskar Hernandez <oskadez@gmail.com>
2025-08-16 21:49:23 -0700

    round state and round info information

--- src/detection/__init__.py
+++ src/detection/__init__.py
@@ -1,1 +1,19 @@
-from .hud_detection import *
+from .hud_detection import (
+    RoundInfo,
+    RoundState,
+    detect_agent_icons,
+    detect_kill_feed,
+    detect_round_info,
+    detect_round_state,
+    is_player_dead,
+)
+
+__all__ = [
+    "detect_agent_icons",
+    "detect_kill_feed",
+    "detect_round_info",
+    "detect_round_state",
+    "is_player_dead",
+    "RoundInfo",
+    "RoundState",
+]

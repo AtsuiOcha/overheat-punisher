29250fa69bd219bd27c6fb8f8cbf0e4e954609d3
Oskar Hernandez <oskadez@gmail.com>
2025-08-22 20:32:22 -0700

    remove need for a detection state

--- src/main.py
+++ src/main.py
@@ -3,43 +3,47 @@ Main loop for real-time overheat detection.
 Target: 20 FPS analysis with smart sampling.
 """
 
-from enum import Enum
-
 from cv2.typing import MatLike
 
 from .analysis import FrameState, detect_overheat, team_diff_at_death
 from .capture import capture_screen
 from .detection import RoundState, detect_round_state, is_player_dead
 
-
-class DetectionState(Enum):
-    MONITORING = 1
-    ANALYZING = 2
+PLAYER_NAME = "malding"
 
 
-def determine_detection_state(
+def get_death_frame_state(
     prev_frame: MatLike,
     frame: MatLike,
-    prev_detection_state: DetectionState,
-) -> DetectionState:
+    prev_death_frame_state: FrameState | None,
+) -> FrameState | None:
+    if prev_death_frame_state:
+        # do some stuff
+        ...
     round_state = detect_round_state(frame=frame)
+    true_team_death = team_diff_at_death(
+        target_player=PLAYER_NAME,
+        prev_frame=prev_frame,
+        cur_frame=frame,
+    )
     # TODO: there might be an issue here when we are mid round but looking at score
     if (
-        prev_detection_state == DetectionState.MONITORING
-        and round_state == RoundState.MID_ROUND
+        round_state == RoundState.MID_ROUND
         and is_player_dead(frame=frame)
-        and team_diff_at_death(prev_frame=prev_frame, cur_frame=frame) >= -1
+        and true_team_death >= -1
     ):
-        return DetectionState.ANALYZING
+        return FrameState(
+            frame=frame,
+            team_diff=true_team_death,
+        )
+    return None
 
-    return DetectionState.MONITORING
 
-
-# might be best to use actual current round to determine round determine when to reset.
+# TODO: get away from using a detection state we can use the existence of a death state
+#   for determing weather we are to look for some overheat event or not
 
 
 def main():
-    detection_state = DetectionState.MONITORING
     death_frame_state: FrameState | None = None
     prev_frame: MatLike | None = None
     while True:
@@ -48,7 +52,7 @@ def main():
 
         # ensure an intial frame has been read
         if prev_frame:
-            curr_detection_state = determine_detection_state(
+            curr_detection_state = get_death_frame_state(
                 prev_frame=prev_frame,
                 frame=cur_frame,
                 prev_detection_state=detection_state,
@@ -59,9 +63,7 @@ def main():
                 detection_state == DetectionState.ANALYZING
                 and curr_detection_state == DetectionState.MONITORING
             ):
-                frame_state = None
                 death_frame_state = None
-                detection_state = curr_detection_state
 
             detection_state = curr_detection_state
             if curr_detection_state == DetectionState.ANALYZING:

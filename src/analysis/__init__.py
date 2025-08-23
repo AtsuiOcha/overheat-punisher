5de6c20d310d07efddc682e65479edbe670aa74b
Oskar Hernandez <oskadez@gmail.com>
2025-08-23 10:42:45 -0700

    cleanup analyzer

--- src/analysis/__init__.py
+++ src/analysis/__init__.py
@@ -1,3 +1,3 @@
-from .game_analyzer import FrameState, detect_overheat, team_diff_at_death
+from .game_analyzer import FrameState, check_overheat, team_diff_at_death
 
-__all__ = ["detect_overheat", "FrameState", "team_diff_at_death"]
+__all__ = ["check_overheat", "FrameState", "team_diff_at_death"]
--- src/analysis/game_analyzer.py
+++ src/analysis/game_analyzer.py
@@ -6,6 +6,7 @@ Detects when monitored player takes unnecessary fights.
 from dataclasses import dataclass, field
 
 from cv2.typing import MatLike
+from loguru import logger
 
 from src.detection import hud_detection
 
@@ -20,23 +21,17 @@ class FrameState:
     killer: str = field(init=False)
 
     def __post_init__(self):
-        round_info = hud_detection.detect_round_info(frame=self.frame) 
-        
+        round_info = hud_detection.detect_round_info(frame=self.frame)
+
         self.round_time_sec = round_info["round_time_sec"]
         if self.team_diff is None:
-            team1, team2 = hud_detection.detect_agent_icons(frame=self.frame)  
+            team1, team2 = hud_detection.detect_agent_icons(frame=self.frame)
             self.team_diff = len(team1) - len(team2)
 
 
-
-@dataclass
-class OverheatEvent:
-    """Detected overheat incident."""
-
-    pass
-
-
-def team_diff_at_death(target_player: str, prev_frame: MatLike, cur_frame: MatLike) -> int:
+def team_diff_at_death(
+    target_player: str, prev_frame: MatLike, cur_frame: MatLike
+) -> int:
     # team diff at prev frame
     team1, team2 = hud_detection.detect_agent_icons(frame=prev_frame)
     prev_team_diff = len(team1) - len(team2)
@@ -48,31 +43,26 @@ def team_diff_at_death(target_player: str, prev_frame: MatLike, cur_frame: MatLi
     if curr_team_diff + 1 == prev_team_diff:
         # player was the only death that occured
         return curr_team_diff
-    
+
     # death event reconstruction until we find score at player death
     kill_feed = hud_detection.detect_kill_feed(frame=cur_frame)
     team_diff_at_event = prev_team_diff
 
     for feed_event in kill_feed:
-        team_diff_at_event = team_diff_at_event - 1 if feed_event["was_team_death"] else team_diff_at_event + 1
+        team_diff_at_event = (
+            team_diff_at_event - 1
+            if feed_event["was_team_death"]
+            else team_diff_at_event + 1
+        )
         if feed_event["victim"].lower() == target_player.lower():
             return team_diff_at_event
 
     return team_diff_at_event
 
 
+def check_overheat(death_frame_state: FrameState, cur_frame_state: FrameState) -> bool:
+    if cur_frame_state.team_diff and death_frame_state.team_diff:
+        return cur_frame_state.team_diff > death_frame_state.team_diff
 
-def has_team_advantage(frame: MatLike) -> bool:
-    """Check if monitored player's team has numbers advantage."""
-    hud_detection.detect_round_info(frame=frame)
-
-    pass
-
-
-def was_death_traded(old_state: FrameState, cur_state: FrameState) -> bool:
-    """Check if death was traded within 3 seconds."""
-    if old_state.team_diff 
-
-def detect_overheat(death_frame_state: FrameState, cur_frame_state: FrameState) -> bool:
-    if death_frame_state.team_diff 
-
+    logger.error(f"team_diffs found to be None {cur_frame_state=} {death_frame_state=}")
+    raise ValueError("team diffs not set properly")
